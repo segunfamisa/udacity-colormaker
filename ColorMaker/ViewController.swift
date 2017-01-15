e59491448bd2ba0b5b45d1a2d34f313b5311a148
Segun Famisa <segunfamisa@gmail.com>
2017-01-15 13:08:16 +0100

    [chore] add UI and code to make ColorMaker app

--- ColorMaker/ViewController.swift
+++ ColorMaker/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // reset sliders
+        resetSliders()
     }
 
 
+    @IBAction func changeColor(_ sender: AnyObject) {
+        mixColors()
+    }
+    
+    func mixColors() {
+        let r: CGFloat = CGFloat(self.redSlider.value)
+        let g: CGFloat = CGFloat(self.greenSlider.value)
+        let b: CGFloat = CGFloat(self.blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
+    
+    func resetSliders() {
+        redSlider.value = 0
+        greenSlider.value = 0
+        blueSlider.value = 0
+        
+        mixColors()
+    }
+    
 }
 
